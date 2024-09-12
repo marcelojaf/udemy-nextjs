b488959cf42c449474b91e458318658e3e1331fa
mjamadorarthrex <marcelo.armador@contractor.arthrex.com>
2024-09-12 19:05:36 -0300

    Criando componente

---
+++ react-crash-course/src/components/Post.jsx
@@ -0,0 +1,11 @@
+const names = ['Marcelo', 'Felipe'];
+
+function Post(){
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+    return <div>
+        <p>{chosenName}</p>
+        <p>React.js is awesome!</p>
+    </div>
+}
+
+export default Post;
