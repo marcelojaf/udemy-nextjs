b37dac2ec7ac7f7be27cf7bfe337770fab402975
mjamadorarthrex <marcelo.armador@contractor.arthrex.com>
2025-04-14 14:41:07 -0300

    Using backend

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
+
+import MainHeader from "./components/MainHeader";
 import PostList from "./components/PostList";
 
 function App() {
-  const posts = [
-    { id: 1, author: "Marcelo", body: "React.js is awesome!" },
-    { id: 2, author: "Lucas", body: "Learning React is fun!" },
-    { id: 3, author: "Gustavo", body: "Components are reusable!" },
-    { id: 4, author: "João", body: "State management is key!" },
-  ];
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   return (
-    <main>
-      <PostList posts={posts} />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
   );
 }
 
--- starting-project/src/components/PostList.jsx
+++ starting-project/src/components/PostList.jsx
@@ -1,17 +1,66 @@
+import { useState, useEffect } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
-import classes from './PostList.module.css';
+import Modal from "./Modal";
+import classes from "./PostList.module.css";
 
 function PostList(props) {
-  const { posts } = props;
+  const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:5175/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  function addPostHandler(postData) {
+    fetch("http://localhost:5175/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
   return (
     <>
-    <NewPost/>
-    <div className={classes.posts}>
-        {posts.map((post) => (
-            <Post key={post.id} author={post.author} body={post.body} />
-        ))}
-    </div>
+      {props.isPosting && (
+        <Modal onClose={props.onStopPosting}>
+          <NewPost
+            onCancel={props.onStopPosting}
+            onAddPost={addPostHandler}
+          />
+        </Modal>
+      )}
+      {!isFetching && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post, index) => (
+            <Post
+              key={index}
+              {...post}
+            />
+          ))}
+        </ul>
+      )}
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start by adding one!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>Loading...</h2>
+        </div>
+      )}
     </>
   );
 }
